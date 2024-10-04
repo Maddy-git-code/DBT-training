f154a6a8005febed5d4fc128a2e0546780da1894
Mathavan S <mathavansnowflake.data@gmail.com>
2024-10-04 14:35:36 +0000

    testing

--- models/models/reporting/report_profit_by_customers.sql
+++ models/models/reporting/report_profit_by_customers.sql
@@ -1,10 +1,3 @@
-select
-customerid,
-segment,
-country,
-sum(orderprofit) as profit,
-from {{ ref('stg_orders') }}
-group by
-customerid,
-segment,
-country
+select customerid, segment, country, sum(orderprofit) as profit,
+from {{ ref("stg_orders") }}
+group by customerid, segment, country
